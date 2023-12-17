3308c5a6e7f058bdfbad5e18d04a37f9a2a38a02
tarik <itarik707@gmail.com>
2023-12-17 10:41:49 +0600

    connect to database

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server is running");
+});
