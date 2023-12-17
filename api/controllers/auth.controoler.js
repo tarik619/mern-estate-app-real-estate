7c07f7acee108cb5c2bd1bf756a564917ff7c8be
tarik <itarik707@gmail.com>
2023-12-17 21:09:12 +0600

    create sign in api route

--- api/controllers/auth.controoler.js
+++ api/controllers/auth.controoler.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,6 +13,23 @@ export const signUp = async (req, res, next) => {
       success: true,
       message: "user created suuccessfully",
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res.cookie("token", token, { httpOnly: true }).status(200).json({ rest });
   } catch (error) {
     next(error);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signUp } from "../controllers/auth.controoler.js";
+import { signUp, signIn } from "../controllers/auth.controoler.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
+router.post("/signin", signIn);
 
 export default router;
