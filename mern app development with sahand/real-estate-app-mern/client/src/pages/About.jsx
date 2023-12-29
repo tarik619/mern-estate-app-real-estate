093a81f317b54c33bfa3f8469519f6f8b242b488
tarik619 <itarik707@gmail.com>
2023-12-29 21:45:51 +0600

    complete about page

--- mern app development with sahand/real-estate-app-mern/client/src/pages/About.jsx
+++ mern app development with sahand/real-estate-app-mern/client/src/pages/About.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="my-20 mx-5 max-w-6xl mx-auto">
+      <h1 className="text-3xl mb-4 text-slate-800 font-bold ">
+        About Tarik Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et officiis
+        eos rerum, delectus totam dicta. Vitae est dolores pariatur praesentium
+        cum nihil numquam vel cumque quia officiis repellat aspernatur
+        repellendus quisquam, corrupti, debitis eaque! Veritatis blanditiis
+        facere sit laboriosam explicabo?
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolorum
+        repudiandae recusandae omnis delectus, corporis suscipit quidem laborum
+        doloribus deleniti voluptatum ut ipsum. Ducimus deserunt quas cum odit.
+        Consectetur reiciendis sapiente reprehenderit incidunt? Temporibus
+        voluptatum sint exercitationem, assumenda rem provident dicta aspernatur
+        molestiae ipsa praesentium dignissimos, possimus explicabo quisquam
+        aliquam.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur,
+        accusantium quos. Ad dignissimos fugiat, maxime molestiae hic tempore
+        eligendi minus nemo, corporis quam eaque iste necessitatibus blanditiis
+        ut, veritatis dolores ullam quod modi incidunt esse numquam. At
+        deserunt, veritatis maiores aliquam ducimus neque harum numquam, quam
+        iure veniam amet voluptate?
+      </p>
+    </div>
+  );
 };
 
 export default About;
